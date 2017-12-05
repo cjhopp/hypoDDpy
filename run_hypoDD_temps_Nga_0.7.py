cb8ca565e4ebc3795a28255242c6755d971b537a
cjhopp <cjhopp@mtu.edu>
2017-12-06 10:10:43 +1300

    still playing with Nga temps weighting and clustering

--- run_hypoDD_temps_Nga_0.7.py
+++ run_hypoDD_temps_Nga_0.7.py
@@ -85,11 +85,11 @@ hypodd_sets = {
     # DAMP = damping. Aim for condition numbers between about 40-80
                     #   Cross-corr Data   #    Catalog Data    #  
              # NITER WTCCP WTCCS WRCC WDCC WTCTP WTCTS WRCT WDCT DAMP 
-    'iters' : ["   5  0.30  -999    2    2   0.7  -999    2    2   200",
-               "   5  0.30  -999    2    1   0.7  -999    2    1   200",
-               "   5  0.50  -999    2    1  0.50  -999    2    1   150",
+    'iters' : ["   5  0.30  -999    2    1   0.7  -999    2    1   150",
+               "   5  0.30  -999    2    1   0.7  -999    2    1   150",
+               "   5  0.50  -999    2    1  0.50  -999    2    1   100",
                "   5  0.70  -999    2  0.5  0.30  -999    2  0.5    50",
-               "   5  1.00  -999    2  0.25 0.01  -999    2  0.5    30"]
+               "   5  1.00  -999    2  0.25 0.01  -999    2  0.5    50"]
             }
 
 ### Cross-correlation Plotting
