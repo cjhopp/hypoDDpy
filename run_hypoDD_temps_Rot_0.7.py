9ef8f62d4cb9ada1182a5e055f96db85c3e6334f
cjhopp <cjhopp@mtu.edu>
2018-02-08 16:45:40 +1300

    new, relocated catalog

--- run_hypoDD_temps_Rot_0.7.py
+++ run_hypoDD_temps_Rot_0.7.py
@@ -10,7 +10,7 @@ from hypoddpy.hypodd_relocator import HypoDDRelocator
 
 # Location of input QML catalog
 # cat_file = '/Volumes/GeoPhysics_07/users-data/hoppche/detections/det_cat_mcc0.4_shift0.2_ALL_LOCATED_uncert0.05.xml'
-cat_file = '/Volumes/GeoPhysics_07/users-data/hoppche/templates/12-15/big_space_clusters/cat_tribe_Rot_stefan_Spicks.xml'
+cat_file = '/Volumes/GeoPhysics_07/users-data/hoppche/templates/12-15/big_space_clusters/cat_tribe_Rot_stefan_Spicks_slocs.xml'
 
 # time slice directory
 wav_dir = '/Volumes/GeoPhysics_07/users-data/hoppche/stefan_sac/SAC/corrected'
