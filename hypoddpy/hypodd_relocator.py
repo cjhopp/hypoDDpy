863d6a68455dc0910630da68c4a2ad87acbcfbe4
cjhopp <cjhopp@mtu.edu>
2018-10-17 18:22:35 +1300

    fix kilometer2degrees spelling

--- hypoddpy/hypodd_relocator.py
+++ hypoddpy/hypodd_relocator.py
@@ -1192,7 +1192,7 @@ class HypoDDRelocator(object):
                                     phase='P', pick_id=pk.resource_id.id,
                                     takeoff_angle=src_dict[int(ev_id)][sta][2],
                                     azimuth=src_dict[int(ev_id)][sta][1],
-                                    distance=kilometer2degree(
+                                    distance=kilometer2degrees(
                                         float(src_dict[int(ev_id)][sta][0]))))
                 event.origins.append(new_origin)
         cat.write(self.output_event_file, format="quakeml")
